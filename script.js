d8dcf50d75ec4faa9fabb1e5d2ce31a5bc43d3c8
MilenaCruzJusta <milenajusta86@gmail.com>
2022-09-22 04:09:57 -0300

    ex5

---
+++ script.js
@@ -0,0 +1,43 @@
+/*Exercicio 1
+const nomeDoProduto = prompt("Insira o nome de um produto")
+let precoDoProduto = Number(prompt("Diga o preço do produto"))
+let valorDoProduto = precoDoProduto - 1
+console.log(`O preço do produto ${nomeDoProduto} é igual a ${valorDoProduto} reais.`)
+
+Exercicio2
+let num1 = Number(prompt("Insira aqui seu primeiro número"))
+let num2 = Number(prompt("Insira aqui seu segundo número"))
+console.log(`Soma:`, num1+num2);
+console.log(`Subtração:`, num1-num2);
+console.log(`Multiplicação:`, num1*num2);
+console.log(`Divisão:`, num1/num2);
+console.log(`Resto da divisão:`, num1%num2);
+
+Exercicio 3
+
+let numeroDaSorte = Number(Math.floor(Math.random() * 10 + 1))
+let numeroDoUsuario = Number(prompt("Insira aqui um número de 1 a 10"))
+
+console.log(`O número sorteado foi: ${numeroDaSorte}`)
+
+if (numeroDoUsuario < numeroDaSorte){
+console.log("Seu número é menor que o número da Sorte!"); 
+} else if (numeroDoUsuario > numeroDaSorte){
+console.log("Seu número é maior que o número da Sorte!");
+} else {(numeroDaSorte === numeroDoUsuario);
+console.log ("Seu número é igual ao Número da Sorte!Parabéns!")
+} 
+
+Exercicio 4
+
+alert("Seja bem vindo(a)! Para sua segurança, responda:")
+let idadeUsuario = Number(prompt("Qual a sua idade?"))
+let alturaUsuario = Number(prompt("Digite sua altura em centímetros"))
+let coraçãoUsuario = prompt("Você tem algum problema cardíaco? Digite S/N")
+    if(idadeUsuario >= 18 && alturaUsuario >= 160 && coraçãoUsuario === "N"){
+    console.log("Radical! Pode entrar.");
+    } else {
+    console.log ("Ops, você não pode entrar, continue se divertindo em outro brinquedo.")
+    }
+
+    Exercicio 5*/
